bc2a0933d03ada2712f8a909ea10af31bce64a71
Buranima <lee123021@gmail.com>
2024-09-13 17:54:59 +0700

    Update esp_readMacAddress.ino

--- esp_readMacAddress/esp_readMacAddress.ino
+++ esp_readMacAddress/esp_readMacAddress.ino
@@ -1,9 +1,3 @@
-/*
-  Rui Santos & Sara Santos - Random Nerd Tutorials
-  Complete project details at https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 #include <WiFi.h>
 #include <esp_wifi.h>
 
@@ -29,5 +23,5 @@ void setup(){
 void loop(){
   Serial.print("[DEFAULT] ESP32 Board MAC Address: ");
   readMacAddress();
-  delay(500);
+  delay(10000);
 }
