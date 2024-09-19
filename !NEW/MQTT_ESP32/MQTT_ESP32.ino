8dbe55ae31f793ae442c2965f2cc97d7d54d67e7
Buranima <lee123021@gmail.com>
2024-09-19 22:14:46 +0700

    all update

---
+++ !NEW/MQTT_ESP32/MQTT_ESP32.ino
@@ -0,0 +1,90 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "esp_wifi.h"
+
+// ข้อมูล WiFi
+const char* ssid = "BURANIMA";
+const char* password = "12345678";
+
+// ข้อมูล MQTT
+const char* mqtt_server = "141.98.17.127";
+const int mqtt_port = 28813;
+const char* mqtt_user = "techlabs";
+const char* mqtt_pass = "ASDzxc!@#QwE123";
+const char* topicSubscribe = "CPS485/ESP32";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+char macAddrMe[18];
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  getMACAddress();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback_mqtt);
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+}
+
+void reconnect_wifi() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi disconnected, reconnecting...");
+    WiFi.disconnect();
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("WiFi reconnected");
+  }
+}
+
+void getMACAddress() {
+  uint8_t mac[6];
+  esp_wifi_get_mac(WIFI_IF_STA, mac);
+  snprintf(macAddrMe, sizeof(macAddrMe), "%02X:%02X:%02X:%02X:%02X:%02X",
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  Serial.print("MAC Address: ");
+  Serial.println(macAddrMe);
+}
+
+void reconnect_mqtt() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(macAddrMe, mqtt_user, mqtt_pass)) {
+      Serial.println("connected");
+      client.subscribe(topicSubscribe);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      delay(5000);
+    }
+  }
+}
+
+void callback_mqtt(char* topicSubscribe, byte* payload, unsigned int length) {
+  String message;
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.println(message);
+}
+
+void loop() {
+  reconnect_wifi();
+  if (!client.connected()) {
+    reconnect_mqtt();
+  }
+  client.loop();
+}
